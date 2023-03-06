4938ddcd656467e23d588525a96abe456db9e8dc
pptaa <chobo05@naver.com>
2023-03-06 19:47:12 +0900

    0307_codingTest Study

---
+++ CodingTest/Public/appleBug_0307/0307_KAKAO_비밀지도.playground/Contents.swift
@@ -0,0 +1,71 @@
+import Foundation
+// 0306_ 18:40 ~ 19:06
+func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
+    var answer: [String] = []
+    
+    var radixArr1 = [String]()
+    var radixArr2 = [String]()
+    
+    for i in arr1 {
+        var radix2 = String(i, radix: 2, uppercase: true)
+        if radix2.count != n {
+            let count = n - radix2.count
+            radix2 = String.init(repeating: "0", count: count) + radix2
+        }
+        radixArr1.append(radix2)
+    }
+    
+    for i in arr2 {
+        var radix2 = String(i, radix: 2, uppercase: true)
+        if radix2.count != n {
+            let count = n - radix2.count
+            radix2 = String.init(repeating: "0", count: count) + radix2
+        }
+        radixArr2.append(radix2)
+    }
+    
+//    let radixArr1 = arr1.map {
+//        var radix2 = String($0, radix: 2, uppercase: true)
+//        if radix2.count != n {
+//            let count = n - radix2.count
+//            radix2 = String.init(repeating: "0", count: count) + radix2
+//        }
+//        return radix2
+//    }
+    
+//    let radixArr2 = arr2.map {
+//        var radix2 = String($0, radix: 2, uppercase: true)
+//        if radix2.count != n {
+//            let count = n - radix2.count
+//            radix2 = String.init(repeating: "0", count: count) + radix2
+//        }
+//        return radix2
+//    }
+    
+    print(radixArr1, radixArr2)
+
+    for i in 0 ..< n {
+        var row = ""
+        for j in 0 ..< n {
+//            print(i, j)
+            
+            if Array(radixArr1[i])[j] == "1" || Array(radixArr2[i])[j] == "1" {
+                row += "#"
+            } else {
+                row += " "
+            }
+        }
+        answer.append(row)
+    }
+    
+    
+    print(answer)
+    return answer
+}
+
+solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28])
+// ["#####","# # #", "### #", "# ##", "#####"]
+
+solution(6, [46, 33, 33 ,22, 31, 50], [27 ,56, 19, 14, 14, 10])
+// ["######", "### #", "## ##", " #### ", " #####", "### # "]
+
---
+++ CodingTest/Public/appleBug_0307/0307_KAKAO_숫자문자열과영단어.playground/Contents.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+// 0306_ 19:30 ~ 19:45
+
+func solution(_ s:String) -> Int {
+    var result = ""
+    let numberList = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+    
+    var startIdx = 0
+    var endIdx = 0
+    let sArray = Array(s)
+    
+    for (idx, cha) in sArray.enumerated() {
+        print(sArray[startIdx...endIdx])
+        if let test = Int(String(cha)) {
+            result += String(cha)
+            startIdx = idx+1
+            endIdx = idx+1
+            continue
+        }
+        
+        if numberList.contains(String(sArray[startIdx...endIdx])) {
+            let number = numberList.firstIndex(of: String(sArray[startIdx...endIdx])) ?? 0
+            result += "\(number)"
+            startIdx = idx+1
+            endIdx = idx+1
+        } else {
+            endIdx = idx+1
+        }
+    }
+    print(result)
+    
+    return Int(result)!
+}
+
+solution("one4seveneight")
+solution("23four5six7")
+solution("2three45sixseven")
+solution("123")
